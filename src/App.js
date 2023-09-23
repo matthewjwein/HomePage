58e82fda410963d278f8125d8b4de6bd44e5f2c3
Matthew Wein <matthewjwein@gmail.com>
2023-09-23 11:07:25 -0400

    Update defaults and add introductory text content

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello, World
+        <p className="Introduction">
+          Hello, World.
+        </p>
+        <p className="Introduction">
+          I'm taking on the challenge of developing a project a week.
+        </p>
+        <p className="Introduction">
+          This website will showcase these projects.
         </p>
       </header>
+      <ol className="ProjectList">
+        <li>Sep 24 - Sep 30: [Meta] Set up this website with React components for Blog entries (tags: React)</li>
+        <li>Oct 1 - Oct 7: Develop a Digital Piano using React Components (tags: React)</li>
+      </ol>
     </div>
   );
 }
