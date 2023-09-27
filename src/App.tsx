c47596f15eb1f8625e49de7c753872c091faf502
Matthew Wein <matthewjwein@gmail.com>
2023-09-27 16:48:07 -0400

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react'
 import ProjectList from './components/ProjectList'
 import Project from './components/Project'
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const projects = [
   {
