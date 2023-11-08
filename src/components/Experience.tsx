372f19c0a3d88100e6ba10563927d548b30afaa4
Matthew Wein <matthewjwein@gmail.com>
2023-11-08 16:17:59 +0000

    Fix lint errors and simplify colors

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -23,7 +23,7 @@ function Experience(): JSX.Element {
                     width={2.5}
                     height={1.65}
                     intensity={65}
-                    color={'#ee7752'}
+                    color={'#24bbe9'}
                     rotation={[- 0.1, Math.PI, 0]}
                     position={[0, 0.55, - 1.15]}
                 />
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import react from '@vitejs/plugin-react'
-import { transformWithEsbuild } from 'vite'
 
 const ViteConfig = {
     root: 'src/',
