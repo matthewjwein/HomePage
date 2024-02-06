b8981f66308e88261a74445c9e6e422a6110a12d
Matthew Wein <matthewjwein@gmail.com>
2024-02-06 15:10:15 -0500

    display resume on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import './App.css'
 import React from 'react'
 import Navbar from './components/Navbar'
-import Project from './components/Project'
-import { ProjectData } from './components/ProjectData'
-import ProjectList from './components/ProjectList'
-import { Route, Routes } from 'react-router';
+import Resume from './components/resume/Resume'
 
 function App() {
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<ProjectList projects={ProjectData} />} />
-        <Route path="/project/1" element={<Project id={1} />} />
-        <Route path="/project/2" element={<Project id={2} />} />
-      </Routes>
+      <Resume />
     </>
   );
 }
